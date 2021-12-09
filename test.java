4a401e553cc986d4435a16574e9f76c0a737abd3
loftunat <95832791+loftunat@users.noreply.github.com>
2021-12-09 17:21:58 -0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,68 @@
+/* void means that the function will not get a response measage back
+package means that the code is interacting in one specific folder
+.out means its send a measage through what comes before the .(System, ect.)
+"int" is a type of variable
+boolwan tells if the statement is true
+Objects(Dates, People, ect.) have one instance for multiple pieces of data*/
+package com.test0001;
+
+public class Main {
+
+public static void main(String [] args){
+     /*byte age=30;
+     long viewsCount= 3_123_456_789l;
+     float price= 10.99f;
+     char letter='A';
+     boolean isEligable= false;*/
+}
+
+
+
+}
+
+//shows the date
+
+package com.test0001;
+
+import java.util.Date;
+
+public class Main {
+
+public static void main(String [] args){
+        /*byte age=30;
+        Date now= new Date();
+        now.get
+        System.out.println("now");*/
+}
+
+
+
+}
+
+//identifies what x and y equals
+package com.test0001;
+
+public class Main;
+
+package static void(String[] args){
+        /*byte x = 1;
+        byte y = x;
+        x=2;
+        System.out.println(y);*/
+}
+
+
+//showing referenses of its original number, ect. and modifying the original number at a certain point/time
+
+package com.test0001;
+
+import java.awt.*;
+
+public class Main;
+
+package static void(String[] args){
+        Point point1 = new Point (x1, y1);
+        Point point2=point1;
+        point1.x=2;
+        System.out.println(point2);
+}
