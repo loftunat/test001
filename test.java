b98755663fedb8d1ee95dc2428118e4b5349a43b
loftunat <95832791+loftunat@users.noreply.github.com>
2021-12-08 19:04:01 -0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,15 @@
+//void means that the function will not get a response measage back
+//package means that the code is interacting in one specific folder
+//.out means its send a measage through what comes before the .(System, ect.)
+
+package com.test0001;
+
+public class Main {
+
+public static void main(String [] args){
+        System.out.println("Hello World");
+}
+
+
+
+}
